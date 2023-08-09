04b23b2a71b66fc4c6ccc7cb02328aa0cb52212b
Alvin <alvin.mantovani@virtualspirit.me>
2023-08-09 15:44:10 +0700

    feat: navigate to create calendar

--- src/pages/Calendar/Components/Cell/index.tsx
+++ src/pages/Calendar/Components/Cell/index.tsx
@@ -1,8 +1,16 @@
 import { useContext, useState } from "react"
 import { CalendarContext } from "../../../../components/CalendarLayout"
+import { useNavigate } from "react-router-dom";
+
 export default ({ day, disabled }: any) => {
-    const { current } = useContext(CalendarContext)
-    return <div className={`${disabled ? "bg-slate-500" : "cursor-pointer hover:bg-sky-300"} flex flex-col items-center w-[calc(100%/7)] h-60 border border-slate-300 p-2`}>
+    const { current } = useContext(CalendarContext);
+    const navigate = useNavigate()
+
+    const createEvent = () => {
+        if(!disabled) navigate("/calendar/create")
+    }
+
+    return <div onClick={createEvent} className={`${disabled ? "bg-slate-500" : "cursor-pointer hover:bg-sky-300"} flex flex-col items-center w-[calc(100%/7)] h-60 border border-slate-300 p-2`}>
         <div className={`${current == day && "bg-blue-500 text-white"} text-bold rounded-full w-5 h-5 flex items-center justify-center`}>{day}</div>
         <div>event</div>
     </div>
